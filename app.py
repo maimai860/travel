a09e431b8152e00518f7ff844baf98cfad41b5b7
maimai860 <maimai7418@outlook.jp>
2026-02-16 10:49:51 +0900

    authentification

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login(location="main")
 
 
 # =========================
